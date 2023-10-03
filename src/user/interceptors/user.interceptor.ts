fc2eb326f0562723de63bbcb63c312e2a77000a6
MouazAlsayyad <103582622+MouazAlsayyad@users.noreply.github.com>
2023-10-03 23:02:27 +0300

    upload app

---
+++ src/user/interceptors/user.interceptor.ts
@@ -0,0 +1,18 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Request } from "express";
+import * as jwt from "jsonwebtoken"
+
+
+export class UserInterceptor implements NestInterceptor {
+  async intercept(
+    context: ExecutionContext,handler:CallHandler
+  ){
+    const request: Request = context.switchToHttp().getRequest()
+    const token = request?.headers?.authorization?.split('Bearer ')[1];
+    const user = await jwt.decode(token) 
+    request['user'] = user
+    return handler.handle()
+  }
+
+  
+}
